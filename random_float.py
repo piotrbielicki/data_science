2637f37f11fc27b122758d8f8abe9e581215afb1
Piotr Bielicki <pantherbielicki2@vp.pl>
2020-04-21 21:45:08 +0200

    random float

---
+++ random_float.py
@@ -0,0 +1,14 @@
+"""
+Import numpy as np.
+Use seed() to set the seed; as an argument, pass 123.
+Generate your first random float with rand() and print it out.
+"""
+
+# Import numpy as np
+import numpy as np
+
+# Set the seed
+np.random.seed(123)
+
+# Generate and print random float
+print(np.random.rand())
